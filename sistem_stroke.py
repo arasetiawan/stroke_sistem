4f29c5dcda52b96e486a726ac35a410bfcc09d7d
arasetiawan <165973879+arasetiawan@users.noreply.github.com>
2025-05-07 08:42:03 +0700

    Update sistem_stroke.py

--- sistem_stroke.py
+++ sistem_stroke.py
@@ -13,25 +13,25 @@ st.title('sistem prediksi stroke')
 col1, col2 = st.columns(2)
 
 with col1 :
-    gender = st.text_input ('Laki-laki/Perempuan 1/0')
+    gender = st.text_input ('Perempuan/Laki-laki 1/0')
 
 with col1 :
     age = st.text_input ('Berapa Umur Anda')
 
 with col1 :
-    hypertension = st.text_input ('apakah anda memiliki riwayat hipertensi Ya/Tidak 1/0')
+    hypertension = st.text_input ('apakah anda memiliki riwayat hipertensi Tidak?Ya 0/1')
 
 with col1 :
-    heart_disease = st.text_input ('apakah anda memiliki riwayat penyakit jantung Ya/Tidak 1/0')
+    heart_disease = st.text_input ('apakah anda memiliki riwayat penyakit jantung Tidak/Ya 1/0')
 
 with col1 :
-    ever_married = st.text_input ('apakah anda pernah menikah Ya/Tidak 1/0')
+    ever_married = st.text_input ('apakah anda pernah menikah Tidak?Ya 1/0')
 
 with col2 :
     work_type = st.text_input ('apa tipe pekerjaan anda? pemerintahan/anak-anak/swasta/wiraswasta 0/1/2/3')
 
 with col2 :
-    Residence_type = st.text_input ('apa tipe tempat tinggal anda pedesaan/perkotaan 0/1')
+    Residence_type = st.text_input ('apa tipe tempat tinggal anda perkotaan/Pedesaan 0/1')
 
 with col2 :
     avg_glucose_level = st.text_input ('berapa kadar glukosa rata-rata anda')

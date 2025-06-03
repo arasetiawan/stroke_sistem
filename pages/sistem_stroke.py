e351177cd1210a77e4473550cac360b01e41f40f
arasetiawan <165973879+arasetiawan@users.noreply.github.com>
2025-06-04 06:26:58 +0700

    Update sistem_stroke.py

--- pages/sistem_stroke.py
+++ pages/sistem_stroke.py
@@ -15,18 +15,18 @@ import streamlit as st
 col1, col2 = st.columns(2)
 
 with col1:
-    gender = 0 if st.radio('Jenis kelamin?', ['Perempuan', 'Laki-laki']) == 'Perempuan' else 1
+    gender = 0 if st.radio('Apa Jenis kelamin Anda?', ['Perempuan', 'Laki-laki']) == 'Perempuan' else 1
     age = st.number_input('Berapa umur anda?', min_value=0, max_value=120)
-    hypertension = 0 if st.radio('Punya riwayat Hipertensi?', ['Tidak', 'Ya']) == 'Tidak' else 1
-    heart_disease = 0 if st.radio('Punya riwayat Jantung?', ['Tidak', 'Ya']) == 'Tidak' else 1
+    hypertension = 0 if st.radio('Apakah Anda Punya riwayat Hipertensi?', ['Tidak', 'Ya']) == 'Tidak' else 1
+    heart_disease = 0 if st.radio('Apakah Anda Punya riwayat Jantung?', ['Tidak', 'Ya']) == 'Tidak' else 1
     ever_married = 0 if st.radio('Apakah anda pernah menikah?', ['Tidak', 'Ya']) == 'Tidak' else 1
 
 with col2:
-    work_type = st.radio('Pilih tipe pekerjaan:', ['Pemerintahan', 'Anak-anak', 'Swasta', 'Wiraswasta'])
-    Residence_type =  0 if st.radio('Pilih tipe tempat tinggal', ['Pedesaan', 'Perkotaan']) == 'Pedesaan' else 1
+    work_type = st.radio('Pilih tipe pekerjaan Anda:', ['Pemerintahan', 'Anak-anak', 'Swasta', 'Wiraswasta'])
+    Residence_type =  0 if st.radio('Pilih tipe tempat tinggal Anda', ['Pedesaan', 'Perkotaan']) == 'Pedesaan' else 1
     avg_glucose_level = st.number_input('Berapa kadar glukosa rata-rata anda?', min_value=0.0, format="%.2f")
     bmi = st.number_input('Berapa berat badan anda?', min_value=0.0, format="%.2f")
-    smoking_status = st.radio('Pilih status merokok:', ['Tidak diketahui', 'Dahulu merokok', 'Tidak pernah merokok', 'Merokok'])
+    smoking_status = st.radio('Pilih status merokok Anda:', ['Tidak diketahui', 'Dahulu merokok', 'Tidak pernah merokok', 'Merokok'])
 
 # Konversi ke nilai numerik menggunakan ternary operator
 work_type = (
@@ -47,7 +47,7 @@ smoking_status = (
 st.write('### Data yang Anda Masukkan:')
 st.write(f"- **Jenis kelamin:** {gender}")
 st.write(f"- **Usia:** {age}")
-st.write(f"- **Hipertensi:** {hypertension}")
+st.write(f"- **Penyakit Hipertensi:** {hypertension}")
 st.write(f"- **Penyakit Jantung:** {heart_disease}")
 st.write(f"- **Status pernikahan:** {ever_married}")
 st.write(f"- **Tipe pekerjaan:** {work_type}")
